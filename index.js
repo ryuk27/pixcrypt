be7537fcec4ea02eb95fec8fc21daf26912824a9
Ram <107495081+ryuk27@users.noreply.github.com>
2024-12-31 18:40:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+/**
+ * pixcrypt
+ * An image encryption node-js cli
+ *
+ * @author ryuk27
+ */
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const encrypt = require('./utils/encrypt');
+const decrypt = require('./utils/decrypt');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear } = flags;
+
+(async () => {
+	init({ clear });
+
+	input.includes(`help`) && cli.showHelp(0);
+	// check if encrypt is present in flags object
+	if (flags.encrypt) {
+		await encrypt(flags);
+	} else if (flags.decrypt) {
+		await decrypt(flags);
+	}
+
+	// footer to show when the program is finished
+
+	const chalk = (await import(`chalk`)).default;
+
+	console.log(
+		chalk.bgMagenta(` Give it a star on github: `) +
+			chalk.bold(` https://github.com/ryuk27/pixcrypt `)
+	);
+})();
